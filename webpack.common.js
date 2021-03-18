68e96d630a4f3678313d6137c501c3ede4da607d
Nikita Peplov <npeplov@yandex.ru>
2021-03-18 16:36:17 +0300

    ttr clean-webpack-plugin from webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -43,7 +42,6 @@ module.exports = {
       template: './src/index.html',
       filename: './index.html',
     }),
-    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[contenthash].css',
       chunkFilename: '[id].css',
